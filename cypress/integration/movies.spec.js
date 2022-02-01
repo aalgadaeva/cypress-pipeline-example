a4179f7df93e07e151cbfea9e52ff2d38bfeeed3
aalgadaeva <57629218+aalgadaeva@users.noreply.github.com>
2022-02-01 12:11:26 +0100

    test

--- cypress/integration/movies.spec.js
+++ cypress/integration/movies.spec.js
@@ -2,6 +2,6 @@
 describe('Movies', () => {
     it('Should list all movies on the home page', () => {
         cy.visit('/')
-        //cy.get(':nth-child(1) > a').should('contain', 'WALL')
+        cy.get(':nth-child(1) > a').should('contain', 'WALL')
     })
 })
